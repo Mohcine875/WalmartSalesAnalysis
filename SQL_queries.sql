73b8fa64bbe803adc37fbd349b4309bf6d3792da
Mohcine Behate <153081796+Mohcine875@users.noreply.github.com>
2026-05-20 00:56:46 -0400

    Remove duplicate SQL query

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -101,15 +101,6 @@ SELECT
 	DISTINCT product_line
 FROM sales;
 
-
--- What is the most selling product line
-SELECT
-	SUM(quantity) as qty,
-    product_line
-FROM sales
-GROUP BY product_line
-ORDER BY qty DESC;
-
 -- What is the most selling product line
 SELECT
 	SUM(quantity) as qty,
